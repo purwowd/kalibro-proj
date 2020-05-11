8c502f8cbfc9f82453ff803280e69994d1bde524
purwowd <id.purwowd@gmail.com>
2020-05-11 02:25:17 -0400

    Refactoring project structure.

---
+++ src/scanner/mcc-mnc/update.py
@@ -0,0 +1,66 @@
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    print("Need BeautifulSoup.\nsudo apt-get install python-bs4")
+    import sys
+    sys.exit()
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+    pythonVersion = "3"
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+    pythonVersion = "2"
+import json
+import io
+
+wikipediaURLs = [
+    'https://en.wikipedia.org/wiki/Mobile_Network_Codes_in_ITU_region_2xx_(Europe)',
+    'https://en.wikipedia.org/wiki/Mobile_Network_Codes_in_ITU_region_3xx_(North_America)',
+    'https://en.wikipedia.org/wiki/Mobile_Network_Codes_in_ITU_region_4xx_(Asia)',
+    'https://en.wikipedia.org/wiki/Mobile_Network_Codes_in_ITU_region_5xx_(Oceania)',
+    'https://en.wikipedia.org/wiki/Mobile_Network_Codes_in_ITU_region_6xx_(Africa)',
+    'https://en.wikipedia.org/wiki/Mobile_Network_Codes_in_ITU_region_7xx_(South_America)'
+]
+
+mcc_codes = {}
+for url in wikipediaURLs:
+    webpage = urlopen(url)
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+for t in soup.find_all("table", class_="wikitable"):
+    try:
+        if "MCC" not in t.text:
+            continue
+        h4 = t.find_previous_sibling("h4")
+        if not h4 or ' - ' not in h4.text or '[edit]' not in h4.text:
+            continue
+        h4 = h4.text.split(' - ')
+        country_name = h4[0]
+        country_code = h4[1][:-6]
+
+        for tr in t.find_all('tr'):
+            td = tr.find_all('td')
+            if not td:
+                continue
+            MCC = td[0].text
+            if not MCC:
+                continue
+            MNC = td[1].text
+            Brand = td[2].text
+            Operator = td[3].text
+            if MCC not in mcc_codes:
+                mcc_codes[MCC] = {}
+
+            mcc_codes[MCC][MNC] = [Brand, Operator, country_name, country_code]
+    except Exception:
+        pass
+
+if mcc_codes:
+    with io.open('mcc_codes.json', 'w', encoding='utf8') as outfile:
+        if pythonVersion == "2":
+            outfile.write(json.dumps(mcc_codes, ensure_ascii=False, encoding="utf-8"))
+        else:
+            outfile.write(json.dumps(mcc_codes, ensure_ascii=False))
